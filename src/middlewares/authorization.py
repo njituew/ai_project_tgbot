4493f992f5b893ab42bf5ad1dd088daab35defea
van-create <ivdmeliseev@edu.hse.ru>
2025-01-26 01:35:21 +0300

    fix critical registration bug

--- src/middlewares/authorization.py
+++ src/middlewares/authorization.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 from aiogram import BaseMiddleware
 from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
 
-from src.registration import EXCEL_FILE
+from src.registration import EXCEL_FILE, RegistrationStates
 
 
 async def check_registered_boolean(user_id: str) -> bool:
@@ -34,6 +35,18 @@ class AuthorizationMiddleware(BaseMiddleware):
 
         if isinstance(event, Message) and event.text == "/start":
             return await handler(event, data)
+        
+        fsm_context: FSMContext = data.get("state")
+        if fsm_context:
+            state = await fsm_context.get_state()
+            if state in [
+                RegistrationStates.waiting_for_name,
+                RegistrationStates.waiting_for_gender,
+                RegistrationStates.waiting_for_age,
+                RegistrationStates.waiting_for_height,
+                RegistrationStates.waiting_for_weight,
+            ]:
+                return await handler(event, data)
                 
         if not await check_registered_boolean(user_id):
             text = "Вы не зарегистрированы. Пожалуйста, зарегистрируйтесь, чтобы пользоваться всеми функциями бота.\n\n/start - начать работу с ботом"
